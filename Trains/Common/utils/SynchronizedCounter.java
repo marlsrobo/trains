ce9ca6c3fcf6b9d226a0b1dc97d0d543dbdc2b42
ronanloughlin <loughlin.r@northeastern.edu>
2021-12-16 07:58:04 -0500

    documented synchronized counter

--- Trains/Common/utils/SynchronizedCounter.java
+++ Trains/Common/utils/SynchronizedCounter.java
@@ -1,6 +1,14 @@
 package utils;
 
+/**
+ * A monotonically increasing integer counter that is both synchronized and safe to use across
+ * threads.
+ *
+ * Copied from the Synchronized Method Oracle Java Tutorial:
+ *  https://docs.oracle.com/javase/tutorial/essential/concurrency/syncmeth.html
+ */
 public class SynchronizedCounter {
+
     private volatile int counter;
 
     public SynchronizedCounter() {
